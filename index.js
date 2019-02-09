8606352fe1b1a5cf7c53a347eab7b441768a644c
JuanC Riera <jc.rierag@gmail.com>
2019-02-08 20:52:49 -0600

    calculadora con suma, resta, multiplicacion y división

---
+++ index.js
@@ -0,0 +1,9 @@
+const operacion = require('./math');
+
+console.log("La suma es: " + operacion.suma(4,5));
+
+console.log("La resta es: " + operacion.resta(4,9));
+
+console.log("La multiplicación es: " + operacion.mult(8,5));
+
+console.log("La división es: " + operacion.div (17,5));
---
+++ math.js
@@ -0,0 +1,15 @@
+
+module.exports = {
+    suma: function (a, b) {
+        return a + b;
+    },
+    resta: function(a, b){
+        return a - b;
+    },
+    mult: function (a, b) {
+        return a * b;
+    },
+    div: function (a, b) {
+        return a / b;
+    }
+}
